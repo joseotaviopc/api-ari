37cbe64375edad2e077d53ffcb14a830f809dd66
José Otavio Carvalho <carvalho.jose@gmail.com>
2025-05-13 15:21:21 -0300

    feat: add auth module

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,25 @@
+//src/auth/auth.module.ts
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { UsersModule } from 'src/users/users.module';
+import { JwtStrategy } from './jwt.strategy';
+import { UsersService } from 'src/users/users.service';
+
+@Module({
+  imports: [
+    PrismaModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN }, // e.g. 30s, 7d, 24h
+    }),
+    UsersModule,
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy, UsersService],
+})
+export class AuthModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,48 @@
+//src/auth/auth.service.ts
+import {
+  Injectable,
+  Logger,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { PrismaService } from './../prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import { AuthEntity } from './entity/auth.entity';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  private readonly logger = new Logger(AuthService.name);
+
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(email: string, password: string): Promise<AuthEntity> {
+    this.logger.log(`Logging in user: ${email}`);
+    // Step 1: Fetch a user with the given email
+    const user = await this.prisma.user.findUnique({ where: { email: email } });
+
+    // If no user is found, throw an error
+    if (!user) {
+      this.logger.log(`No user found for email: ${email}`);
+      throw new NotFoundException(`No user found for email: ${email}`);
+    }
+
+    // Step 2: Check if the password is correct
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    // If password does not match, throw an error
+    if (!isPasswordValid) {
+      this.logger.log(`Invalid password for user: ${email}`);
+      throw new UnauthorizedException('Invalid password');
+    }
+
+    // Step 3: Generate a JWT containing the user's ID and return it
+    this.logger.log(`User logged in successfully: ${email}`);
+    return {
+      accessToken: this.jwtService.sign({ userId: user.id }),
+    };
+  }
+}
